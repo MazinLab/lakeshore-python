65f836fe06fb687d09d16fbdf9c279b6612448e4
jtb <bloo1987@proton.me>
2024-08-06 14:48:45 -0700

    boilerplate

---
+++ lakeshore/model_340.py
@@ -0,0 +1,5 @@
+"""Implements functionality unique to the Lake Shore Model 372 AC bridge and temperature controller."""
+from .model_340_enums import Model340Enums
+from .temperature_controllers import TemperatureController, StandardEventRegister, OperationEvent
+from .generic_instrument import RegisterBase
+
---
+++ lakeshore/model_340_enums.py
@@ -0,0 +1,6 @@
+"""Implements a class containing the enums relevant to the Model 372."""
+from enum import IntEnum, Enum
+
+
+class Model340Enums:
+    pass
