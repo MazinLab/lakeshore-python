2dc01e52a08d37af8d22d416d43b907d72fb0c8c
jtb <bloo1987@proton.me>
2024-08-08 09:41:22 -0700

    fixed vid_pid mutation after initial test

--- lakeshore/model_340.py
+++ lakeshore/model_340.py
@@ -1,5 +1,5 @@
 """Implements functionality unique to the Lake Shore Model 340 AC bridge and temperature controller.
-NOTE: This is an incomplete implmentation that only supports serial, be sure to check the enums to make sure the desired device setting is defined.
+NOTE: This is an incomplete implmentation that only supports serial.
 """
 from .model_340_enums import Model340Enums
 from .temperature_controllers import TemperatureController, StandardEventRegister
@@ -11,7 +11,7 @@ Model340StandardEventRegister = StandardEventRegister
 ## Unique register layouts ##
 class Model340StatusByteRegister(RegisterBase):
     """
-    Status byte register  is read-only and has flags signaling
+    Status byte register is read-only and has flags signaling
     specific reports by the system (Pg. 9-3), if enabled via the
     Service Request Enable Register. The Serivce Request Enable
     Register is isomorphic and will be aliased (it's just read/write).
@@ -54,12 +54,10 @@ class Model340(TemperatureController, Model340Enums):
     """
     Implements the Model 340 high-level API. Incomplete, check enums.
     """
-    vid_pid = []
-
     # Initialize registers
     _status_byte_register = Model340StatusByteRegister
     _service_request_enable = Model340ServiceRequestEnable
-    
+
     def __init__(self,
                  baud_rate: int,
                  vid: hex,
@@ -70,16 +68,12 @@ class Model340(TemperatureController, Model340Enums):
                  ip_address: None = None, # Not supported on 340
                  tcp_port: None = None, # Not supported on 340
                  **kwargs):
+        
+        # Set instance value of vid_pid since this can change across 340s (as opposed to being the same
+        # for other devices)
+        self.vid_pid = [(vid, pid)]
 
-        # Call the parent init, then fill in values specific to the 372
+        # Call the parent init, then fill in values specific to the 340
         TemperatureController.__init__(self, serial_number, com_port, baud_rate, timeout, ip_address,
                                        tcp_port, **kwargs)
-        
-        # Set the vid_pid INSTANCE value. This is different from USB/TCP implementations
-        # since although the VID/PID value for a SKU is common to all instances of the SKU, they have a
-        # unique serial number to help uniquely identify an instance in cases where multiple devices 
-        # connected to one PC.
-        # In this case, we're assuming the 340 will be connected by a serial-to-USB adapter, which
-        # could appear as a different VID/PID depending on the model (serial number is still necessary
-        # in cases of duplicate adapters!)
-        self.vid_pid = [(vid, pid)]
+    
